a0c2d98cbfcaf90759da964e56937cd2dcf3e987
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-12 02:34:53 +0100

    UPDATE program.cs

--- 0x00-polygon/Program.cs
+++ 0x00-polygon/Program.cs
@@ -6,7 +6,15 @@ namespace _0x00_polygon
     {
         static void Main()
         {
-          
+            messages msg = new();
+            msg.display(0);
+
+            regularPolygon polygon = new();
+            msg.display(1);
+            polygon.sides = Convert.ToUInt16(Console.ReadLine());
+
+            int intAng = polygon.interiorAng();
+            msg.display(2, intAng);
         }
     }
 }
