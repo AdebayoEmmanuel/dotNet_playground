214599aa359c496e6fa9a8344857c66023c6de72
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-12 01:43:41 +0100

    UPDATE README.md

---
+++ 0x00-polygon/regularPolygon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0x00_polygon
+{
+    class regularPolygon
+    {
+        //Attributes of a regular polygon
+        public int length, sides;
+
+        public void interiorAng()
+        {
+            double interiorAngle = (sides - 2) * 180;
+            Console.WriteLine("Sum of the interior angle of this Polygon is: {0}", interiorAngle);
+        }
+    }
+}
