d114a5bf21293d5c8df46e8f00c0ce0605a815a6
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-12 02:33:52 +0100

    CREATE messages.cs

---
+++ 0x00-polygon/messages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0x00_polygon
+{
+    class messages
+    {
+        const string welcomeMsg = "Welcome to the Polygon App";
+        const string inputPrompt = "Please enter number of sides: ";
+        const string solutionMsg = "Sum of the interior angle of this Polygon is: {0}";
+        public void display(int msgIndex, int? intAng = null)
+        {
+            switch (msgIndex)
+            {
+                case 0:
+                    Console.WriteLine(welcomeMsg);
+                    break;
+                case 1:
+                    Console.WriteLine(inputPrompt);
+                    break;
+                case 2:
+                    Console.WriteLine(solutionMsg, intAng);
+                    break;
+
+            }
+        }
+    }
+}
